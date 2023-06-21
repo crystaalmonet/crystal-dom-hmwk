18835d54a49e794740a19759ceb14a9f3f141880
crystaalmonet <81581068+crystaalmonet@users.noreply.github.com>
2023-06-21 00:39:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -17,32 +17,73 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
+let titleEl = getElementById('main-title');
+  titleEl.innerHTML = <h1> home </h1>
 
   // Part 2
-
+let bodyEl = document.body;
+  bodyEl.style.backgroundColor = 'hotpink';
 
   // Part 3
+// Select the DOM's Favorite Things list
+let favoriteThingsList = document.querySelector('#favorite-things');
+
+let lastListItem = favoriteThingsList.lastElementChild;
+favoriteThingsList.removeChild(lastListItem);
 
 
   // Part 4
-
-
+let specialTitle = document.querySelectorAll('.special-title');
+  for (let i=0; i < specialTitle.length; i++) {
+    specialTitle[i].style.fontSize = '2rem';
+  }
   // Part 5
-
+let pastRaces = document.querySelector('#past-races');
+  let chiCity = pastRaces.querySelector('li:nth-child(4)');
+  pastRaces.removeChild(chiCity);
 
   // Part 6
-
-
+let newCityItem = document.createElement('li');
+  let newCity = 'Miami';
+  newCityItem.textContent = newCity;
+pastRaces.appendChild(newCityItem);
   // Part 7
+
+let mainContainer = document.querySelector('.main');
+let newBlogPost = document.createElement('div');
+newBlogPost.classList.add('blog-post');
+let newTitle = document.createElement('h2');
+newTitle.textContent = 'Miami'; 
+
+let newText = document.createElement('p');
+newText.textContent = 'I DROVE MY CAR THROUGH THE VERSACE MANSION!'; 
+
+newBlogPost.appendChild(newTitle);
+newBlogPost.appendChild(newText);
+mainContainer.appendChild(newBlogPost);
 
 
   // Part 8
-
+  const randomQuote = function() {
+  document.querySelector('#quote-of-the-day').innerText = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
+};
+  let quoteTitle = document.querySelector('#quote-title');
+ randQuote.addEventListener('click', function() {
+   randomQuote(); 
+ });
 
   // Part 9
 
-
+let blogPost = document.querySelectorAll('.blog-post')
+  blogPosts.forEach(function(blogPost) {
+  blogPost.addEventListener('mouseout', function() {
+    blogPost.classList.toggle('purple');
+  });
+  blogPost.addEventListener('mouseenter', function() {
+    blogPost.classList.toggle('red');
+  });
+});
+    
 
 
 });
